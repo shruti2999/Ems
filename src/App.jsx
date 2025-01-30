d7c5691d89aeca2cede7a897b7794caf297aa70f
shruti2999 <shrutikumari9208@gmail.com>
2025-01-30 08:34:33 +0530

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import Login from "./components/Auth/login";
+import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
+import AdminDashboard from "./components/Dashboard/AdminDashboard";
+
+import { AuthContext } from "./context/AuthProvider";
 
 const App = () => {
+  const [user, setUser] = useState(null);
+  const authData = useContext(AuthContext);
+
+  useEffect(()=>{
+    if(authData){
+      const loggedInUser=localStorage.getItem("loggedInUser")
+      if(loggedInUser){
+        setUser(loggedInUser.role)
+      }
+    }
+  },[authData]);
+
+  const handleLogin = (email, password) => {
+    if (email == 'admin@me.com' && password == '123') {
+      setUser('admin')
+      localStorage.setItem('loggedInUser', JSON.stringify({ role: 'admin' }))
+    } else if (userData) {
+      const employee = userData.find((e) => email == e.email && e.password == password)
+      if (employee) {
+        setUser('employee')
+        setLoggedInUserData(employee)
+        localStorage.setItem('loggedInUser', JSON.stringify({ role: 'employee',data:employee }))
+      }
+    }
+    else {
+      alert("Invalid Credentials")
+    }
+  }
+
+
+  const data = useContext(AuthContext);
+  console.log(data);
   return (
     <div>
-     
+      {!user ? <Login handleLogin={handleLogin} /> : " "}
+      {user == "admin" ? <AdminDashboard /> : <EmployeeDashboard />}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/TaskList/AcceptTask.jsx
+++ src/components/TaskList/AcceptTask.jsx
@@ -1,1 +1,9 @@
+ import React from 'react'
  
+ const AcceptTask = () => {
+   return (
+     <div>AcceptTask</div>
+   )
+ }
+ 
+ export default AcceptTask
